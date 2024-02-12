eb6c74ab932381833de2788734b6ec965fad4a5b
maiduong <duongmdph40323@gmail.com>
2024-02-12 14:11:35 +0700

    update outPage register

--- app/client/views/user/script.blade.php
+++ app/client/views/user/script.blade.php
@@ -1,26 +1,28 @@
 <script>
-    var modal = document.querySelector('.modal-overlay');
-    var closeModal = document.querySelector('.close-modal');
-    var navbar = document.querySelector('.navbar'); // Tham chiếu đến navbar
+    document.addEventListener('DOMContentLoaded', function () {
+        var modal = document.querySelector('.modal-overlay');
+        var closeModal = document.querySelector('.close-modal');
+        var navbar = document.querySelector('.navbar');
 
-    closeModal.onclick = function() {
-        modal.style.display = "none";
-        navbar.style.display = "flex"; // Hiển thị lại navbar
-        window.location.href = '/'; // Chuyển hướng về trang chủ
-    }
-
-    window.onclick = function(event) {
-        if (event.target == modal) {
+        closeModal.addEventListener('click', function() {
             modal.style.display = "none";
-            navbar.style.display = "flex"; // Hiển thị lại navbar
-        }
-    }
+            navbar.style.display = "flex";
+            window.history.back();
+        });
 
-    var openModalBtn = document.querySelector('.open-modal-btn');
-    if (openModalBtn) { // Kiểm tra nếu nút mở modal tồn tại
-        openModalBtn.onclick = function() {
-            modal.style.display = "flex";
-            navbar.style.display = "none"; // Ẩn navbar
+        window.addEventListener('click', function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+                navbar.style.display = "flex";
+            }
+        });
+
+        var openModalBtn = document.querySelector('.open-modal-btn');
+        if (openModalBtn) {
+            openModalBtn.addEventListener('click', function() {
+                modal.style.display = "flex";
+                navbar.style.display = "none";
+            });
         }
-    }
+    });
 </script>
